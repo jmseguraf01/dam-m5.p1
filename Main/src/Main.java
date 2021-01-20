9c9ee4e03d31c0c11a31314aeed63dafd201adb3
jmseguraf01 <jmseguraf01@elpuig.xeill.net>
2021-01-20 20:20:00 +0100

    Primer commit

--- Main/src/Main.java
+++ Main/src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("cambiado");
-        System.out.println("secuent");
+        System.out.println("hola mon");
     }
 }
